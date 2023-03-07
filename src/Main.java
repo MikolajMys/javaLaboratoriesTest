507e9851de8c14d5387ecb30995964620d424ad7
miki <mikolaj.my@gmail.com>
2023-03-07 16:26:23 +0100

    zadanie 1 lab-07-03

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,12 @@
-/*Zadanie 1.
-Zdefiniuj klasę Point posiadającą dwa publiczne, ostateczne pola x, y.
-Napisz konstruktor ustawiający te wartości.
-
-Zdefiniuj klasę Segment reprezentującą odcinek, posiadającą dwa prywatne punkty klasy Point.
-Wygeneruj akcesory i mutatory klasy Segment. Napisz publiczną metodę, która zwraca długość odcinka.
-W kolejnym kroku usuń mutatory i utwórz konstruktor ustawiający
-te pola na wartości swoich dwóch parametrów.
-
-Zadanie 2.
-Zdefiniuj w klasie Segment publiczną metodę toSvg(),
-która zwróci napis zawierający kod języka znacznikowego SVG pozwalający wyświetlić tę linię.*/
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         Point point = new Point(1,1);
         Segment segment = new Segment(point, new Point(2,2));
         System.out.println(segment.getSvg());
+        List<String> cars = new ArrayList<String>();
+
     }
 }
