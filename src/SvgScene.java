50ddbaff49db3be8f1bdc5722edde7d55fa82957
miki <mikolaj.my@gmail.com>
2023-03-07 16:58:15 +0100

    zadanie 2 lab-07-03

---
+++ src/SvgScene.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.io.FileWriter;
+public class SvgScene {
+    int width = 0, height = 0;
+    ArrayList<Polygon> polygons = new ArrayList<>();
+    private void addPolygon(Polygon polygon){
+        polygons.add(polygon);
+        Point point = polygon.getMaxCords();
+        height = (int)Math.max(point.y, height);
+        width = (int)Math.max(point.x, width);
+    }
+    public void save(String fileName){
+        try {
+            FileWriter fw = new FileWriter(fileName);
+            String html = " <html>\n" + "<body>\n" + "svg width=\" "+ width +" \" height=\""+ height +"\">\n";
+            for(Polygon polygon : polygons){
+                html += polygon.toSvg();
+            }
+            html+= "</svg>\n" + "\n" + "</body>\n" + "</html> ";
+            fw.close();
+        }
+        catch (Exception e){
+
+        }
+    }
+}
