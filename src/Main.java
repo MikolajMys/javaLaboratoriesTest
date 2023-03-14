e8128221b50c2be53608fa1caf7a5072b311f1c7
miki <mikolaj.my@gmail.com>
2023-03-14 18:06:43 +0100

    zadanie 3 lab-14-03

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
         Shape poly = new Polygon(new Vec2[]{new Vec2(120,60), new Vec2(270,280), new Vec2(240,320), new Vec2(110,80)});
         Shape ellipse = new Ellipse(new Vec2(3.14, 14.3), 100, 100);
         poly = new SolidFillShapeDecorator(poly, "green");
+        poly = new StrokeShapeDecorator(poly, "blue", "10");
         ellipse = new SolidFillShapeDecorator(ellipse, "red");
         SvgScene scene=new SvgScene();
         scene.addShape(poly);
---
+++ src/StrokeShapeDecorator.java
@@ -0,0 +1,14 @@
+public class StrokeShapeDecorator extends ShapeDecorator{
+    private String color;
+    private String width;
+
+    public StrokeShapeDecorator(Shape decoratedShape, String color, String width) {
+        super(decoratedShape);
+        this.color = color;
+        this.width = width;
+    }
+    @Override
+    public String toSvg(String parameters){
+        return super.toSvg("stroke=\""+this.color+"\"stroke=\""+this.width+"\""+parameters);
+    }
+}
