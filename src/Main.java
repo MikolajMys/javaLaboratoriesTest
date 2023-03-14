7d881a495b103e84232c1c160ee05bbdbafd9199
miki <mikolaj.my@gmail.com>
2023-03-14 18:05:17 +0100

    zadanie 2 lab-14-03 v2

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,13 @@ import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-        Shape poly = new SolidFilledPolygon("red", new Vec2[]{new Vec2(120,60), new Vec2(270,280), new Vec2(240,320), new Vec2(110,80)});
-
+        Shape poly = new Polygon(new Vec2[]{new Vec2(120,60), new Vec2(270,280), new Vec2(240,320), new Vec2(110,80)});
+        Shape ellipse = new Ellipse(new Vec2(3.14, 14.3), 100, 100);
+        poly = new SolidFillShapeDecorator(poly, "green");
+        ellipse = new SolidFillShapeDecorator(ellipse, "red");
         SvgScene scene=new SvgScene();
         scene.addShape(poly);
+        scene.addShape(ellipse);
         scene.saveHtml("scene.html");
     }
 }
---
+++ src/SolidFillShapeDecorator.java
@@ -0,0 +1,12 @@
+public class SolidFillShapeDecorator extends ShapeDecorator{
+    private String color;
+
+    public SolidFillShapeDecorator(Shape decoratedShape, String color) {
+        super(decoratedShape);
+        this.color = color;
+    }
+    @Override
+    public String toSvg(String parameters){
+        return super.toSvg("fill=\""+this.color+"\""+parameters);
+    }
+}
