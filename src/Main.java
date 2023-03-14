763ba07c1869a465dd55335d7eb078215c809e89
miki <mikolaj.my@gmail.com>
2023-03-14 17:30:49 +0100

    zadanie 1 lab-14-03

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,10 @@
 
-/*Przeanalizuj zmiany, jakie zostały poczynione względem programu napisanego na poprzednich zajęciach:
-
-konstruktor klasy Polygon ustawia tablicę wierzchołków,
-usunięte zostały wszystkie pola i metody związane ze stylem,
-klasa abstrakcyjna Shape mogła stać się przez to interfejsem,
-klasa Point została przemianowana na Vec2.*/
 import java.util.ArrayList;
 import java.util.Locale;
 
 public class Main {
-    public static final Point test = new Point(2,3);
-
-
-
     public static void main(String[] args) {
-        Shape poly = new Polygon(new Vec2[]{new Vec2(120,60), new Vec2(270,280), new Vec2(240,320), new Vec2(110,80)});
+        Shape poly = new SolidFilledPolygon("red", new Vec2[]{new Vec2(120,60), new Vec2(270,280), new Vec2(240,320), new Vec2(110,80)});
 
         SvgScene scene=new SvgScene();
         scene.addShape(poly);
