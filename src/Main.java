3e7e8407a1eae641b9755427be73bae8939be6a7
miki <mikolaj.my@gmail.com>
2023-04-04 18:38:56 +0200

    zadanie 4 lab-04-04 issues

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,13 @@ public class Main {
         customList.addFirst("test2");
 
         System.out.println(customList.getFirst());
+        String test3 = "";
+        Double test4 = 2.0;
+
+        Class t1 = test3.getClass();
+
+        if(t1.equals(test4.getClass())) {
+
+        }
     }
 }
