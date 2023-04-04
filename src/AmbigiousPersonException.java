694b679f8e112a0cab6571d1b98a53270f433a5e
miki <mikolaj.my@gmail.com>
2023-04-04 09:00:00 +0200

    lab-28-03

--- src/AmbigiousPersonException.java
+++
@@ -1,9 +0,0 @@
-public class AmbigiousPersonException extends Exception {
-    String path1;
-    String path2;
-    public AmbigiousPersonException(String message, String path1, String path2) {
-        super(message);
-        this.path1=path1;
-        this.path2=path2;
-    }
-}
